81d400d04eeca4ec3c596b6c0f7b2e040bccc6c7
free21k <eclipse@ahope.co.kr>
2023-01-08 09:50:33 +0900

    add: Main
    - get Item 추가
    - update Item 추가

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import Union
+
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: Union[str, None] = None,
+    item: Union[Item, None] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
